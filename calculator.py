eb3481f82c1416602d5be3096715bbaa3174f895
Philippe Fernandez <mahagony4@gmail.com>
2021-11-28 14:23:18 +0100

    Add division

--- calculator.py
+++ calculator.py
@@ -16,3 +16,8 @@ def subtract(first_term, second_term):
 def multiply(first_term, second_term):
     """Multiplication"""
     return first_term * second_term
+
+
+def division(first_term, second_term):
+    """Division"""
+    return first_term / second_term
